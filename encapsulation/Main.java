066fce07e3938547778dc4e19296e2da06cae0e4
hamzehziad34 <hamzehziad34@gmail.com>
2025-09-01 16:42:49 +0300

    Add files via upload

---
+++ encapsulation/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encapsulation;
+
+/**
+ *
+ * @author hamze
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();  // Default constructor
+
+        // Set values using setters
+        p.setName("Alice");
+        p.setAge(25);
+
+        // Display info
+        p.displayInfo();
+    
+    }
+    
+}
---
+++ encapsulation/Person.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encapsulation;
+
+/**
+ *
+ * @author hamze
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    // Default constructor
+    public Person() {
+        // You can leave it empty or set default values
+        this.name = "";
+        this.age = 0;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
