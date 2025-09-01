bae1ec62f8fc4b03102ec90c59802bfec0dbef89
hamzehziad34 <hamzehziad34@gmail.com>
2025-09-01 16:17:25 +0300

    Add files via upload

---
+++ inheritance/main.java
@@ -0,0 +1,14 @@
+
+package inheritance;
+
+
+public class main {
+    
+     public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();   // Inherited method
+        d.bark();  // Child method
+    }
+    
+    
+}
